f07324d56d29f799b85e185b76bdea174ac89b1b
oxalica <oxalicc@pm.me>
2019-10-30 19:06:40 +0800

    Specify cpu in qemu

--- target.nix
+++ target.nix
@@ -2,12 +2,12 @@ with import <nixpkgs/lib>; {
 
   aarch64-unknown-linux-gnu = {
     platform = systems.examples.aarch64-multiplatform;
-    qemuFlags = "-M virt -cpu max";
+    qemuFlags = "-M virt -cpu cortex-a72";
   };
 
   armv7l-unknown-linux-gnueabihf = {
     platform = systems.examples.armv7l-hf-multiplatform;
-    qemuFlags = "-M virt -cpu max";
+    qemuFlags = "-M virt -cpu cortex-a15";
   };
 
 }
